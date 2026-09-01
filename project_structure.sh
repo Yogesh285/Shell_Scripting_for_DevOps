17f63b4a752d2ae1f69fca0048e05452b17afb5f
yogesh ingavale <ingavaleyogesh2@gmail.com>
2026-09-01 23:09:45 +0530

    Create project structure with directories
    This script creates a basic project structure with directories for scripts, logs, and configuration.

---
+++ project_structure.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PROJECT="my-first-project"
+
+mkdir "$PROJECT"
+mkdir "$PROJECT/scripts"
+mkdir "$PROJECT/logs"
+mkdir "$PROJECT/config"
+
+echo "Project structure created successfully with the help of script"
+
+echo "Project Name: $PROJECT"
