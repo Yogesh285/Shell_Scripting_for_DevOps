feb7650ee71b58d394f3a4fe4ac64bcd1b755321
yogesh ingavale <ingavaleyogesh2@gmail.com>
2026-09-03 11:19:00 +0530

    Add error_handling.sh with create_directory function
    Initial implementation of error handling script with a placeholder function for creating directories.

---
+++ error_handling.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+create_directory() {
+        
+}
+
+
+
+
+# 3xpected output:: root@ip-172-31-8-232:~/shell-script-project# vi error_handling.sh
+#root@ip-172-31-8-232:~/shell-script-project# rm -r demo
+#root@ip-172-31-8-232:~/shell-script-project# ./error_handling.sh
+#This should not work because the code is interrupted
+#root@ip-172-31-8-232:~/shell-script-project# ls
+#demo  error_handling.sh  scripting-project.sh
+#root@ip-172-31-8-232:~/shell-script-project# ./error_handling.sh
+#mkdir: demo: File exists
+#The code is being exited as the directory already exist
+#root@ip-172-31-8-232:~/shell-script-project#
