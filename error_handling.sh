eeb575bd889257059d7af5ca1c6d16d75421f0a7
yogesh ingavale <ingavaleyogesh2@gmail.com>
2026-09-03 11:22:00 +0530

    Add the expected output after successfully execute code in linux server
    Updated comments to clarify expected output for the script.

--- error_handling.sh
+++ error_handling.sh
@@ -13,7 +13,9 @@ echo "This should not work because the code is interrupted"
 
 
 
-# 3xpected output:: root@ip-172-31-8-232:~/shell-script-project# vi error_handling.sh
+# Expected output::
+
+#root@ip-172-31-8-232:~/shell-script-project# vi error_handling.sh
 #root@ip-172-31-8-232:~/shell-script-project# rm -r demo
 #root@ip-172-31-8-232:~/shell-script-project# ./error_handling.sh
 #This should not work because the code is interrupted
